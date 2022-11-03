0742235f0c82575688a6b7422c1858ce229f28d4
AnkitMathur-1 <69048087+AnkitMathur-1@users.noreply.github.com>
2022-11-03 10:59:32 +0530

    Update hashSetIterator.java

--- java_module/HashSet/src/hash/hashSetIterator.java
+++ java_module/HashSet/src/hash/hashSetIterator.java
@@ -1,5 +1,41 @@
 package hash;
 
 public class hashSetIterator {
-
+public static void main(String[] args)
+    {
+  
+        
+        HashSet<String> h = new HashSet<String>();
+  
+       
+        h.add("India");
+        h.add("Australia");
+        h.add("South Africa");
+  
+      
+        h.add("India");
+  
+        
+        System.out.println(h);
+        System.out.println("List contains India or not:"
+                           + h.contains("India"));
+  
+       
+        h.remove("Australia");
+        System.out.println("List after removing Australia:"
+                           + h);
+  
+        
+        System.out.println("Iterating over list:");
+  
+       
+        Iterator<String> i = h.iterator();
+  
+       
+        while (i.hasNext())
+  
+            // Iterating over elements
+            // using next() method
+            System.out.println(i.next());
+    }
 }
